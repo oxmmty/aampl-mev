8fa93a9858094cc5ca77e6a47865f15c7964e38b
oxmmty <oxmmty@gmail.com>
2025-03-14 21:05:29 +0900

    feat:view

--- src/View.sol
+++ src/View.sol
@@ -28,7 +28,6 @@ interface ILendingPool {
 }
 
 
-
 interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
     function transfer(address to, uint256 amount) external returns (bool);
